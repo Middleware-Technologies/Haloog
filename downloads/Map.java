c4025b88d92084db3c43989a538f093048c68932
Paolo Manca <paolo@manca.eu>
2015-07-31 07:48:25 -0700

    Added mapreduce for the video download counting task

---
+++ downloads/Map.java
@@ -0,0 +1,37 @@
+package haloog.downloads;
+
+import haloog.LogEntry;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+
+public class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+
+	private final static IntWritable one = new IntWritable(1);
+	private Text outKey;	
+	private LogEntry logEntry;
+
+	private DateFormat keyDateFormat = new SimpleDateFormat("yyyyMMdd");
+
+	@Override
+	public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+		
+		logEntry = new LogEntry(value.toString());
+		
+		outKey = new Text(keyDateFormat.format(logEntry.getLoggedTime()));
+		
+		if (logEntry.getLoggedRequest().contains(".wmv")) {
+			output.collect(outKey, one);
+		}
+	}
+
+}
